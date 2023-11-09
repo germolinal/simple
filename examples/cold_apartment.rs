ebc397609acdc34f5488596cc4fa14edffa819c8
German Molina <germolinal@gmail.com>
2023-11-10 10:53:21 +1300

    added cold_apartment in project examples

---
+++ examples/cold_apartment.rs
@@ -0,0 +1,33 @@
+use simple::{run_simulation::*, Model};
+
+
+fn main() -> Result<(), String> {
+    // cargo instruments --template 'CPU Profiler' --release --example cold_apartment 
+
+    let options = SimOptions {
+        input_file: "./tests/cold_apartment/cold.spl".into(),
+        weather_file: "./tests/wellington.epw".into(),
+        output: Some("./tests/cold_apartment/check.csv".into()),
+        control_file: None,
+        research_mode: false,
+        n: 4,
+    };
+
+    // Create model
+    let (simple_model, mut state_header) = Model::from_file(&options.input_file)?;
+
+    let controller = simple::void_control::VoidControl {};
+
+    let res = &options.output.clone().ok_or("No output")?;
+    let out = std::fs::File::create(res).map_err(|e| e.to_string())?;
+    run(
+        &simple_model,
+        &mut state_header,
+        // state,
+        &options,
+        out,
+        controller,
+    )?;
+
+    Ok(())
+}
